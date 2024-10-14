c25a79fd50b8b7a73086f6585e09ebd34b6ac117
“shubham71099” <shubham71099@gmail.com>
2024-10-14 21:08:57 +0530

    Edit profile API Updated

--- routes/user.js
+++ routes/user.js
@@ -21,4 +21,40 @@ router.get("/profile", async (req, res) => {
   }
 });
 
+router.put("/profile", async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+      return res.status(400).json({ message: "Name and email are required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser && existingUser._id.toString() !== userId) {
+      return res
+        .status(400)
+        .json({ message: "Email is already in use by another account." });
+    }
+
+    const user = await User.findOneAndUpdate(
+      { _id: userId },
+      { name, email },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({
+      message: "Profile updated successfully",
+      email: user.email,
+      name: user.name,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
