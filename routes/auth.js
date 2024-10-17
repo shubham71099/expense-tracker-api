2dce401f16c66372d596863e1b09bcad7a2128c6
“shubham71099” <shubham71099@gmail.com>
2024-10-17 19:47:32 +0530

    Token validity Updated

--- routes/auth.js
+++ routes/auth.js
@@ -41,7 +41,7 @@ router.post("/login", async (req, res) => {
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
+      expiresIn: "365d",
     });
     res.json({ token });
   } catch (error) {
